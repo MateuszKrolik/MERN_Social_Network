e6c4b877ee7503366cadc921ab1b8fad208e398c
Mateusz <mateuszkrolik87@gmail.com>
2024-01-24 23:14:46 +0100

    2nd Commit
    Set up basic Node Express REST API and added GET and POST routes
    - Defined the basic setup for a new Node Express REST API.
    
    - Added a GET route in the controller to return dummy data in JSON format.
    
    - Added a POST route to simulate the creation of a new post.
    
    - Used the body-parser middleware to parse incoming JSON data from requests.
    
    - Set up appropriate status codes for responses (200 for GET, 201 for POST).
    
    - Tested the API endpoints using Postman by sending requests and verifying the responses.

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express'); //import express
+const bodyParser = require('body-parser');
 
 const feedRoutes = require('./routes/feed');
 
 const app = express(); //create express app, by executing express function
 
-app.use('/feed', feedRoutes); 
+// app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
+app.use(bodyParser.json()); // application/json
+
+
+app.use('/feed', feedRoutes);
 
 app.listen(8080); //will need 3000 later
--- controllers/feed.js
+++ controllers/feed.js
@@ -1,3 +1,22 @@
 exports.getPosts = (req, res, next) => {
-    
+    res.status(200).json({
+        //200=success
+        posts: [
+            {
+                title: 'First Post',
+                content: 'This is the first post!',
+            },
+        ],
+    });
+};
+
+exports.createPost = (req, res, next) => {
+    const title = req.body.title;
+    const content = req.body.content;
+    // Create post in db
+    res.status(201).json({
+        //201=success, resource created
+        message: 'Post created successfully!',
+        post: { id: new Date().toISOString(), title: title, content: content },
+    });
 };
