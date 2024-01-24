7292c04109be28b752a9eb5652c93683e3056556
Mateusz <mateuszkrolik87@gmail.com>
2024-01-25 00:06:59 +0100

    3rd Commit
    Implemented CORS headers in Express server
    - Added a middleware function in the Express server to set CORS headers on all responses.
    
    - The 'Access-Control-Allow-Origin' header was set to '*' to allow access from any client.
    
    - The 'Access-Control-Allow-Methods' header was set to 'OPTIONS, GET, POST, PUT, PATCH, DELETE' to allow these HTTP methods.
    
    - The 'Access-Control-Allow-Headers' header was set to 'Content-Type, Authorization' to allow these headers in the request.
    
    - This setup allows the server to respond to requests from different origins, which is necessary when the client and the server are not served from the same domain.

--- app.js
+++ app.js
@@ -8,6 +8,18 @@ const app = express(); //create express app, by executing express function
 // app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
 app.use(bodyParser.json()); // application/json
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*'); //allow access to any client
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+    ); //allow these methods
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Content-Type, Authorization'
+    ); //allow these headers
+    next();
+});
 
 app.use('/feed', feedRoutes);
 
