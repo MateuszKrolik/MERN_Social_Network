f539eee3ac0ad3836f2ef4d492ba80654c625a22
Mateusz <mateuszkrolik87@gmail.com>
2024-02-13 20:20:15 +0100

    7th Commit feat: Add MongoDB connection and Post model
    - Installed mongoose package to connect to MongoDB and define models.
    - Imported mongoose in app.js and established a connection to MongoDB Atlas using the connection string.
    - Added error handling for the database connection. If the connection fails, the error is logged to the console.
    - Moved the server start logic (app.listen) into the .then block of the database connection to ensure the server only starts if the database connection is successful.
    - Created a new folder 'models' and added a new file 'post.js' to define the Post model.
    - In post.js, imported mongoose and extracted the Schema constructor.
    - Defined a new Post schema with fields for title, imageUrl, content, and creator. All fields are of type string and are required.
    - Added a timestamps option to the Post schema. When set to true, mongoose automatically adds createdAt and updatedAt fields to the schema.
    - Exported a mongoose model based on the Post schema. The model is named 'Post', which will result in a 'posts' collection in the MongoDB database.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-const express = require('express'); //import express
+const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const feedRoutes = require('./routes/feed');
 
@@ -9,18 +10,24 @@ const app = express(); //create express app, by executing express function
 app.use(bodyParser.json()); // application/json
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*'); //allow access to any client
-    res.setHeader(
-        'Access-Control-Allow-Methods',
-        'OPTIONS, GET, POST, PUT, PATCH, DELETE'
-    ); //allow these methods
-    res.setHeader(
-        'Access-Control-Allow-Headers',
-        'Content-Type, Authorization'
-    ); //allow these headers
-    next();
+  res.setHeader('Access-Control-Allow-Origin', '*'); //allow access to any client
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+  ); //allow these methods
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); //allow these headers
+  next();
 });
 
 app.use('/feed', feedRoutes);
 
-app.listen(8080); //will need 3000 later
+mongoose
+  .connect(
+    'mongodb+srv://mateuszkrolik87:1I9UbNZqMksVzkNk@cluster0.gdjmk4f.mongodb.net/messages'
+  )
+  .then((result) => {
+    app.listen(8080);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
