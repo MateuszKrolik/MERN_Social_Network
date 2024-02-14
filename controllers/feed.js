5a1663d4562b3206ab6e11f3975161d05c857136
Mateusz <mateuszkrolik87@gmail.com>
2024-02-14 18:41:30 +0100

    14th Commit Implemented pagination for posts
    - Added pagination logic to the frontend in the loadPosts method in feed.js
    - Added a page query parameter to the URL when fetching posts
    - In the getPosts controller action, extracted the current page from the request query parameters
    - Set a default value for the current page to 1 and the number of posts per page to 2
    - Added a new database query to count the total number of posts using countDocuments
    - Stored the total number of posts in a variable, totalItems
    - Modified the existing database query to fetch posts to implement pagination
    - Used the skip method to skip the posts that were on the previous pages
    - Used the limit method to limit the number of posts retrieved to the number of posts per page
    - In the response, returned not only the posts but also the total number of posts
    - Updated the frontend to handle the totalItems value returned from the backend and show next and previous buttons accordingly
    - Tested the pagination by adding multiple posts and navigating between pages

--- controllers/feed.js
+++ controllers/feed.js
@@ -7,11 +7,23 @@ const Post = require('../models/post');
 const post = require('../models/post');
 
 exports.getPosts = (req, res, next) => {
+  const currentPage = req.query.page || 1;
+  const perPage = 2;
+  let totalItems;
   Post.find()
+    .countDocuments()
+    .then((count) => {
+      totalItems = count;
+      return Post.find()
+        .skip((currentPage - 1) * perPage)
+        .limit(perPage);
+    })
     .then((posts) => {
-      res
-        .status(200)
-        .json({ message: 'Fetched posts successfully.', posts: posts });
+      res.status(200).json({
+        message: 'Fetched posts successfully.',
+        posts: posts,
+        totalItems: totalItems,
+      });
     })
     .catch((err) => {
       if (!err.statusCode) {
