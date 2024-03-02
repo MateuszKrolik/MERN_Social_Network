dd13d97039b7aaf63face6dc322e46e2fccafc53
Mateusz <mateuszkrolik87@gmail.com>
2024-03-02 22:30:49 +0100

    35th Commit Refactor auth middleware tests using Sinon
    - Imported Sinon for stubbing JWT verify method in auth middleware tests.
    - Replaced JWT verify method with a stub that returns a predefined user ID.
    - Added a test to ensure that the JWT verify method is called during the authentication process.
    - Restored the original JWT verify method after each test to ensure the integrity of other tests.
    - Ensured that all tests pass after refactoring.

--- server/test/auth-middleware.mjs
+++ server/test/auth-middleware.mjs
@@ -1,4 +1,6 @@
 const { expect } = await import('chai');
+import jwt from 'jsonwebtoken';
+import sinon from 'sinon';
 
 import authMiddleware from '../middleware/is-auth.js';
 
@@ -10,6 +12,21 @@ describe('Auth Middleware', () => {
     expect(authMiddleware.bind(this, req, {}, () => {})).to.throw(
       'Not authenticated.'
     );
+  });
+
+  it('should yield a userId after decoding the token', () => {
+    const req = {
+      get: (headerName) => {
+        return 'Bearer xyz';
+      },
+    };
+    sinon.stub(jwt, 'verify');
+    jwt.verify.returns({ userId: 'abc' });
+    authMiddleware(req, {}, () => {});
+    expect(req).to.have.property('userId');
+    expect(req).to.have.property('userId', 'abc');
+    expect(jwt.verify.called).to.be.true;
+    jwt.verify.restore();
   });
 
   it('should throw an error if the authorization header is only one string', () => {
@@ -24,7 +41,7 @@ describe('Auth Middleware', () => {
 
 // Auth Middleware
 // ✔ should throw an error if no authorization header is present
+// ✔ should yield a userId after decoding the token
 // ✔ should throw an error if the authorization header is only one string
 
-
-// 2 passing (4ms)
+// 3 passing (6ms)
